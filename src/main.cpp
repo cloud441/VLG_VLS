63ab7def85afccfaa76bed3a6c2e70a665ee8697
Kevin Guillet <kevin.guillet@epita.fr>
2021-05-26 16:15:02 +0200

    [ARCHI]: add main and igraph test code

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <igraph.h>
+
+
+
+int main(int argc, char **argv)
+{
+    if (argc < 1)
+    {
+        perror("Error: not enough argument to main.");
+        return 1;
+    }
+
+
+    return 0;
+}
